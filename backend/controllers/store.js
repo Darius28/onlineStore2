4d417ef82f5ad3ebed524840f0485252c7493739
Darius28 <likith.dm.28@gmail.com>
2021-08-31 16:42:13 +0530

    hover image changes

--- backend/controllers/store.js
+++ backend/controllers/store.js
@@ -1,11 +1,12 @@
 import Shop from "../models/shop";
+import Item from "../models/item";
 var mongoose = require("mongoose");
 
 export const selectedItem = async (req, res) => {
   try {
     console.log(req.params.itemId);
     var objectId = mongoose.Types.ObjectId(req.params.itemId);
-    const item = await Shop.findOne({ "items._id": objectId });
+    const item = await Item.findOne({ _id: objectId });
     console.log(item);
     res.send({ item });
   } catch (err) {
--- frontend/src/components/Item/Item.js
+++ frontend/src/components/Item/Item.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import "./Item.css";
 import axios from "axios";
@@ -6,19 +6,68 @@ import { BackendUrl } from "../../utils/BackendUrl";
 
 export default function Item() {
   const history = useHistory();
+  const [itemData, setItemData] = useState();
+  const [selectedImage, setSelectedImage] = useState();
+  const [prevHoverImg, setPrevHoverImg] = useState();
+  const [hoverImg, setHoverImg] = useState();
 
   useEffect(() => {
     const getItemData = async () => {
       const { data } = await axios.get(
         `${BackendUrl}${history.location.pathname}`
       );
+      console.log(data.item);
+      setItemData(data.item);
+      setSelectedImage(data.item.pictures[0].Location);
     };
-    getItemData();
+    if (history) {
+      getItemData();
+    }
   }, [history]);
+
+  const selectedImageHandler = (picture) => {
+    setSelectedImage(() => {
+      return picture;
+    });
+    if (prevHoverImg !== undefined) {
+      console.log("lolol")
+    }
+    const hoveredImg = document.getElementById(`${picture}`);
+    console.log(hoveredImg);
+    hoveredImg.classList.add("image-border");
+    setHoverImg(hoveredImg);
+  };
+
+  const removeBorderHandler = () => {
+    console.log("hoverImg on mouse remove: ", hoverImg);
+    setPrevHoverImg(hoverImg);
+  };
 
   return (
     <div className="item-container">
-      <div></div>
+      <div className="item-image-container">
+        <div className="item-preview-container">
+          {itemData
+            ? itemData.pictures.map((pic) => {
+                return (
+                  <img
+                    id={pic.Location}
+                    width={108}
+                    height={54}
+                    src={pic.Location}
+                    onMouseOver={selectedImageHandler.bind(null, pic.Location)}
+                    onMouseLeave={removeBorderHandler}
+                  />
+                );
+              })
+            : null}
+        </div>
+        <div className="item-images-container">
+          {selectedImage ? (
+            <img width={486} height={243} src={selectedImage} />
+          ) : null}
+        </div>
+      </div>
     </div>
   );
 }
